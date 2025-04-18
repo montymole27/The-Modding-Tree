7f353690b13ddd38a774bccd9aca76352e358f71
montymole27 <nathaniel.j.rittner@gmail.com>
2025-04-18 15:09:32 -0400

    update

--- js/mod.js
+++ js/mod.js
@@ -42,7 +42,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasUpgrade('p', 11)) gain = gain.add(1)
+	if (hasUpgrade('u', 11)) gain = gain.add(1)
 	return gain
 }
 
