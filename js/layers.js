96f849364755ec877e3b70aff1434138e8b8a0e2
montymole27 <nathaniel.j.rittner@gmail.com>
2025-04-18 15:08:24 -0400

    update

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+addLayer("u", {
+    name: "Upgrades", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "$", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -22,14 +22,14 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "U", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "U", description: "U: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
     upgrades: {
         11: {
-            title: "",
-            description: "Double your point gain.",
+            title: "UPGRADE I",
+            description: "Add 1 to point gain.",
             cost: new Decimal(1)
-             }
+            }
     }
 })
