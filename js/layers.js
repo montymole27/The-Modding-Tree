a2f078f164f290971e8ae414ee69be5ccc805cca
montymole27 <nathaniel.j.rittner@gmail.com>
2025-04-21 09:50:06 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -156,5 +156,46 @@ addLayer("p", {
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effe
             },
+        25: {
+            title: "UPGRADE X",
+            description: "Unlock RESETS",
+            cost: new Decimal(500),
+            unlocked() {
+                let bool = false;
+                if (hasUpgrade('p', 24)) bool = true
+                return bool;
+            },
+            },
         }
 })
+
+addLayer("r", {
+    name: "Resets", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#787878",
+    requires: new Decimal(1000), // Can be a function that takes requirement increases into account
+    resource: "Resets", // Name of prestige currency
+    baseResource: "$", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        let mult = new Decimal(1)
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "r", description: "r: Reset for resets", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade('p', 25)},
+    upgrades: {
+        }
+})
+
