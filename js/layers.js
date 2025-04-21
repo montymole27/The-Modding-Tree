299ec5728d90d21e1323258f7a112bb95a15f77c
montymole27 <nathaniel.j.rittner@gmail.com>
2025-04-21 10:33:14 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,3 +1,5 @@
+var testTree = [["p"],["r"]]
+
 addLayer("p", {
     name: "Upgrades", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "$", // This appears on the layer's node. Default is the id with the first letter capitalized
